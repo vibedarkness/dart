e63419b411bed576cb0b10ab0c94ba405f765950
omzo <omarthioms35@gmail.com>
2022-08-13 19:34:40 +0000

    test

---
+++ index.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  int a = 25;
+  double b = 45.25;
+  int c = 12;
+
+  String d = "bonjour vibe le resultat de votre calcul est egal a :";
+
+  int f = b.toInt();
+  print(f);
+  print(d);
+  print(a * b + c);
+}
