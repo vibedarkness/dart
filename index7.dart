99840b529f1f0b4de8c5460115948fc3475fec1a
omzo <omarthioms35@gmail.com>
2022-08-14 02:47:11 +0000

    test

---
+++ index7.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  Map monMap = new Map();
+  monMap["username"] = "omzo";
+  monMap["password"] = "123456789";
+
+  var devMap = {
+    "utilisateur": "dingo",
+    "mot de passe": "darkness",
+  };
+  monMap["email"] = "omzo@gmailk.com";
+  print(devMap);
+  print(monMap);
+}
